c9a68e008844da42da716f85fcd424d2d9d14d04
Jonathan Pham <phamjonathan364@outlook.com>
2023-12-05 23:38:45 -0800

    cleaned up code

--- foodie.py
+++ foodie.py
@@ -1,45 +1,32 @@
 
-# used this resource to help hide my API Key
 # https://medium.com/black-tech-diva/hide-your-api-keys-7635e181a06c
+# Used to get api key inside of config.py
 import config
 
-# Using Google Maps Python Client on Github
 # https://github.com/googlemaps/google-maps-services-python
 import googlemaps
 
-import time
+import time,os
 
-import os
-
-# Conncect with API key 
 gmaps = googlemaps.Client(key=config.apiKey)
 
-
-# This function takes a zipCode and a keyword string as a parameter
-# This function uses the Google Maps API to first take the zipcode that the user enters
-# And then converts it to Lat/Long coordinates in order to use the Nearby Search function
+# This function uses the Google Maps API for Nearby Search function
 # The function will return a JSON object that we can use to extract names of places
 def processList(zipCode, keyword):
  
-    # Using this geocode function in order to get the JSON object to get the latitude and longitude 
+    # Geocode function to get the JSON object 
     geocode_result = gmaps.geocode(zipCode)
-
     lat = geocode_result[0]["geometry"]["location"]["lat"]
     long = geocode_result[0]["geometry"]["location"]["lng"]
     
-    # for the places.nearby() function to work, the location has to be in a string.
-    #  So we are combining the lat/long into a string separated by a comma. 
+    # For the places.nearby() function to work, the location has to be in a string.
     location = str(lat) + "," + str(long)
 
     # Checks for places nearby within 10km
     places = gmaps.places_nearby(location, 10000, keyword)
     
-    # Return JSON object 
     return places
   
-
-# This function takes in a zip code and then checks if the length of the input is 5
-# In addition, it will check if the input are all digits
 # Returns a boolean value false if not a valid zip code
 def zipCodeCheck(zipCode):
     if(len(zipCode) != 5):
@@ -48,39 +35,37 @@ def zipCodeCheck(zipCode):
         return False
     return True
 
-# This function enters the keyword input page for the user
-# The user can type keywords, which will be added to a list
-# Once the user decides to finish adding keywords, the funciton will 
-# convert the list objects and converts it into a string to be processed as a keyword 
-# for the processList() function.
-def keywordMode(keyWordString):
 
-    # Initialize list
-    keywordList = []
-
+def keywordPrint():
     print("\nYou entered keyword mode! Commands below:")
     print("Enter any word to input keywords")
     print("'e' to exit keyboard input")
     print("'li' to see list of keywords")
     print("'u' to undo your last filter keyword")
 
-    # Make an infinite loop for users to continuously add keywords until exit
+# Note: Cannot split else ifs
+def keywordMode(keyWordString):
+    keywordList = []
+    keywordPrint()
+
+    # Users can continuously add keywords until exit
     while True:
         keywordInput = input("User Input -- ")
         
-        # When the user exists
+        # When the user exits
         if keywordInput == 'e':
 
-            # Concatenate keyWordString to be returned 
-            for i in keywordList:
-                keyWordString = keyWordString + ", " + i
+            # Convert keywordList into keywordString to be returned 
+            # for the processList() function.
+            for keyword in keywordList:
+                keyWordString = keyWordString + ", " + keyword
             print("Exited Keyword mode")
             break
 
         # When the user undos a keyword
         elif keywordInput == 'u':
             
-            # This makes sure that nothing bad happens if you pop an empty list
+            # Prevents errors if you pop an empty list
             if len(keywordList) == 0:
                 continue 
             else:
@@ -90,42 +75,40 @@ def keywordMode(keyWordString):
         elif keywordInput == 'li':
             print(keywordList)
 
-            # If no commands are put, everything else is a keyword
         else:
-            keywordList.append(keywordInput)
+            keywordList.append(keywordInput) # If no commands are put, everything else is a keyword
                 
     return keyWordString
 
-def genPlanmode(zipCode): 
-    print("\nYou entered planning mode!")
-
-    numPlaces = None
-    planKeywords = []
-
-    # This double checks for number input 
+def checkNumInput():
+    numPlaces = 0
     while True: 
         numPlaces = input("Enter how places you would like to visit (MAX 5): ")
         if numPlaces.isdigit() == False:
             continue
-        if (int(numPlaces) <= 5) & (int(numPlaces) > 0):
+        if (int(numPlaces) <= 5) & (int(numPlaces) > 0): # between 1-5
             break
+    return numPlaces
 
-    # This loop will ask the user for a keyword for each place
-    for i in range(int(numPlaces)):
-        print("\nPlace " + str(i + 1) + "...")
+# Creates keyword list to be processed by API
+def keywordCreate(numPlaces, planKeywords):
+    for place in range(int(numPlaces)):
+        print("\nPlace " + str(place + 1) + "...")
         currKeyword = input("What are you feeling? (enter keyword): ")
+        currKeyword = "Restaurant," + currKeyword
         planKeywords.append(currKeyword)
+    return planKeywords
 
-    # For each keyword inputted, it will process a list to be appended to a text file starting from 1-5
-    for i in range(int(numPlaces)):
-        currList = processList(zipCode, planKeywords[i])
-        createPath = "planner/" + str(i+1) + ".txt"
+def processPlannerFiles(numPlaces, planKeywords, zipCode):
+
+    # Adds list of restaurants in x.txt files inside of planner directory
+    for place in range(int(numPlaces)):
+        currList = processList(zipCode, planKeywords[place])
+        createPath = "planner/" + str(place+1) + ".txt"
         currString = ""
 
-        # print(createPath)
-
-        for j in currList["results"]:
-            currString = currString + (j["name"]) + "~"
+        for entry in currList["results"]:
+            currString = currString + (entry["name"]) + "~"
 
         # https://www.w3schools.com/python/ref_string_rstrip.asp
         # This is to remove the trailing comma 
@@ -136,27 +119,57 @@ def genPlanmode(zipCode):
         planner.write(currString)
         planner.close()
 
-        
-
-    # Once all the places have been written out on the files
-    # You can now 
-    # Then, create a request.txt that is empty 
+def createRequest():
     request = open("request.txt", "w")
     request.write("")
     request.close()
-    return int(numPlaces)
-            
-    
 
+
+def genPlanmode(zipCode): 
+    print("\nYou entered planning mode!")
+
+    numPlaces = None
+    planKeywords = []
+
+    numPlaces = checkNumInput()
+
+    # Ask the user for a keyword for each place they want to visit
+    keywordCreate(numPlaces, planKeywords)
+
+    # For each keyword inputted, it will process a list to be appended to a text file starting from 1-5
+    processPlannerFiles(numPlaces, planKeywords, zipCode)
+
+    createRequest()
+    return int(numPlaces)
+
+def printPlan(numPlaces):
+    print("Here is your generated plan!\n ")
+    for planI in range(numPlaces):
+        
+        header = str(planI + 1) + ". "
+        place = planResult.readline()
+        place = place.rstrip('\n')
+        print(header + place)
+            
 
 
 if __name__ == "__main__":
-    print("Welcome to Foodie!")
+
+    # https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+    print("""                                      
+                                                   ,---. 
+            ,------.                ,--.,--.       |   | 
+            |  .---',---.  ,---.  ,-|  |`--' ,---. |  .' 
+            |  `--,| .-. || .-. |' .-. |,--.| .-. :|  |  
+            |  |`  ' '-' '' '-' '\ `-' ||  |\   --.`--'  
+            `--'    `---'  `---'  `---' `--' `----'.--.  
+                                                   '--'
+          """)
 
     # Automatic keyword for restaurant to dial in places
     keywords = "Restaurant"
 
-    # While loop to check for valid zip code
+    # Check for valid zip code
     while True:
         zipCode = input("Please enter a zip code: ")
         zipCheck = zipCodeCheck(zipCode)
@@ -165,22 +178,20 @@ if __name__ == "__main__":
         else:
             print("Please try again!\n")
     
+    # User input loops indefinitely
     while True:
         print("\nList:")
         
+        # Process and print the list of places found
         currList = processList(zipCode, keywords)
 
-        # Print the list of places found
         for i in currList["results"]:
                 print(i["name"])
 
-        # Available commands for now
         print("\nEnter 'k' to enter new keywords to filter results")
         print("Enter 'p' to generate a plan for the day!")
         print("Enter 'x' to exit program")
 
-        # Make this program go indefinitely, but there will be an exit condition for the user
-        # which is pressing x
         listInput = input("User Input -- ")
 
         # Enters keyword mode, clears any exisiting keyword to reset
@@ -188,6 +199,7 @@ if __name__ == "__main__":
             keywords = "Restaurant"
             keywords = keywordMode(keywords)
 
+        # Enters planning mode
         elif listInput == 'p':
 
             numPlaces = genPlanmode(zipCode)
@@ -195,18 +207,12 @@ if __name__ == "__main__":
             time.sleep(4)
             planResult = open("output.txt", "r")
             
-            print("Here is your generated plan!\n ")
-            for planI in range(numPlaces):
-
-                header = str(planI + 1) + ". "
-                place = planResult.readline()
-                place = place.rstrip('\n')
-                print(header + place)
+            printPlan(numPlaces)
             
             input("\nPress enter to continue")
             os.remove("request.txt")
 
-        # Exit
+        # Exits program
         elif listInput == 'x':
             print("\nThanks for using Foodie!")
             exit()
